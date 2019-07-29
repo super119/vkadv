db96f96aa15f6ccc3e37bef4f76bb5bb710c78dd
Mark Zhang <ace119@163.com>
2019-07-29 11:27:19 +0800

    Create instance/device/queue, create buffer next
    Signed-off-by: Mark Zhang <ace119@163.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use vulkano::instance::Instance;
+use vulkano::instance::InstanceExtensions;
+use vulkano::instance::PhysicalDevice;
+use vulkano::device::Device;
+use vulkano::device::DeviceExtensions;
+use vulkano::device::Features;
+
 fn main() {
-    println!("Hello, world!");
+    let instance = Instance::new(None, &InstanceExtensions::none(), None)
+                             .expect("failed to create instance");
+    let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
+    for family in physical.queue_families() {
+        println!("Found a queue family with {:?} queue(s)", family.queues_count());
+    }
+    let queue_family = physical.queue_families().find(|&q| q.supports_graphics())
+                       .expect("couldn't find a graphical queue family");
+    let (device, mut queues) = {
+        Device::new(physical, &Features::none(), &DeviceExtensions::none(),
+        [(queue_family, 0.5)].iter().cloned()).expect("failed to create device")
+    };
+    let queue = queues.next().unwrap();
 }
