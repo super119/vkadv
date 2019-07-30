854ed64b4259e106b55ecf3dbe0ade2185d93c00
Mark Zhang <ace119@163.com>
2019-07-30 16:57:45 +0800

    Adding hello graphics - a minimal graphics Vulkan program
    Signed-off-by: Mark Zhang <ace119@163.com>

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,11 @@ use vulkano::format::Format;
 use vulkano::image::Dimensions;
 use vulkano::image::StorageImage;
 use vulkano::format::ClearValue;
+use vulkano::framebuffer::Framebuffer;
+use vulkano::pipeline::GraphicsPipeline;
+use vulkano::framebuffer::Subpass;
+use vulkano::command_buffer::DynamicState;
+use vulkano::pipeline::viewport::Viewport;
 
 fn copy_buffer() {
     let instance = Instance::new(None, &InstanceExtensions::none(), None)
@@ -147,8 +152,133 @@ fn hello_image() {
     image.save("image.png").unwrap();
 }
 
+#[derive(Default, Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+vulkano::impl_vertex!(Vertex, position);
+
+fn hello_graphics() {
+    let instance = Instance::new(None, &InstanceExtensions::none(), None)
+                             .expect("failed to create instance");
+    let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
+    for family in physical.queue_families() {
+        println!("Found a queue family with {:?} queue(s)", family.queues_count());
+    }
+    let queue_family = physical.queue_families().find(|&q| q.supports_graphics())
+                       .expect("couldn't find a graphical queue family");
+    let (device, mut queues) = {
+        Device::new(physical, &Features::none(), &DeviceExtensions::none(),
+        [(queue_family, 0.5)].iter().cloned()).expect("failed to create device")
+    };
+    let queue = queues.next().unwrap();
+
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+    let vertex_buffer = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(),
+                                                       vec![vertex1, vertex2, vertex3].into_iter()).unwrap();
+
+    mod vs {
+        vulkano_shaders::shader!{
+            ty: "vertex",
+            src: "
+#version 450
+layout(location = 0) in vec2 position;
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+}"
+        }
+    }
+
+    mod fs {
+        vulkano_shaders::shader!{
+            ty: "fragment",
+            src: "
+#version 450
+layout(location = 0) out vec4 f_color;
+void main() {
+    f_color = vec4(1.0, 0.0, 0.0, 1.0);
+}"
+        }
+    }
+
+    let vs = vs::Shader::load(device.clone()).expect("failed to create shader module");
+    let fs = fs::Shader::load(device.clone()).expect("failed to create shader module");
+
+    let render_pass = Arc::new(vulkano::single_pass_renderpass!(device.clone(),
+        attachments: {
+            color: {
+                load: Clear,
+                store: Store,
+                format: Format::R8G8B8A8Unorm,
+                samples: 1,
+            }
+        },
+        pass: {
+            color: [color],
+            depth_stencil: {}
+        }
+    ).unwrap());
+
+    let image = StorageImage::new(device.clone(), Dimensions::Dim2d { width: 1024, height: 1024 },
+                                  Format::R8G8B8A8Unorm, Some(queue.family())).unwrap();
+    let buf = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(),
+                                             (0 .. 1024 * 1024 * 4).map(|_| 0u8))
+                                             .expect("failed to create buffer");
+    let framebuffer = Arc::new(Framebuffer::start(render_pass.clone())
+                               .add(image.clone()).unwrap()
+                               .build().unwrap());
+    let pipeline = Arc::new(GraphicsPipeline::start()
+        // Defines what kind of vertex input is expected.
+        .vertex_input_single_buffer::<Vertex>()
+        // The vertex shader.
+        .vertex_shader(vs.main_entry_point(), ())
+        // Defines the viewport (explanations below).
+        .viewports_dynamic_scissors_irrelevant(1)
+        // The fragment shader.
+        .fragment_shader(fs.main_entry_point(), ())
+        // This graphics pipeline object concerns the first pass of the render pass.
+        .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+        // Now that everything is specified, we call `build`.
+        .build(device.clone())
+        .unwrap());
+    let dynamic_state = DynamicState {
+        viewports: Some(vec![Viewport {
+            origin: [0.0, 0.0],
+            dimensions: [1024.0, 1024.0],
+            depth_range: 0.0 .. 1.0,
+        }]),
+        .. DynamicState::none()
+    };
+
+    let command_buffer = AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family()).unwrap()
+        .begin_render_pass(framebuffer.clone(), false, vec![[0.0, 0.0, 1.0, 1.0].into()])
+        .unwrap()
+
+        .draw(pipeline.clone(), &dynamic_state, vertex_buffer.clone(), (), ())
+        .unwrap()
+
+        .end_render_pass()
+        .unwrap()
+
+        .copy_image_to_buffer(image.clone(), buf.clone())
+        .unwrap()
+
+        .build()
+        .unwrap();
+
+    let finished = command_buffer.execute(queue.clone()).unwrap();
+    finished.then_signal_fence_and_flush().unwrap().wait(None).unwrap();
+
+    let buffer_content = buf.read().unwrap();
+    let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
+    image.save("triangle.png").unwrap();
+}
+
 fn main() {
     // copy_buffer();
     // hello_shader();
-    hello_image();
+    // hello_image();
+    hello_graphics();
 }
