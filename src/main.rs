0b71342f2968e5bfe77bf64fa8d3b7e41cc62890
Mark Zhang <ace119@163.com>
2019-07-30 13:48:38 +0800

    Adding codes of hello image(GPU fills an image)
    Signed-off-by: Mark Zhang <ace119@163.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+use image::{ImageBuffer, Rgba};
 use vulkano::instance::Instance;
 use vulkano::instance::InstanceExtensions;
 use vulkano::instance::PhysicalDevice;
@@ -12,6 +13,10 @@ use vulkano::command_buffer::CommandBuffer;
 use vulkano::sync::GpuFuture;
 use vulkano::pipeline::ComputePipeline;
 use vulkano::descriptor::descriptor_set::PersistentDescriptorSet;
+use vulkano::format::Format;
+use vulkano::image::Dimensions;
+use vulkano::image::StorageImage;
+use vulkano::format::ClearValue;
 
 fn copy_buffer() {
     let instance = Instance::new(None, &InstanceExtensions::none(), None)
@@ -111,7 +116,39 @@ void main() {
     println!();
 }
 
+fn hello_image() {
+    let instance = Instance::new(None, &InstanceExtensions::none(), None)
+                             .expect("failed to create instance");
+    let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
+    for family in physical.queue_families() {
+        println!("Found a queue family with {:?} queue(s)", family.queues_count());
+    }
+    let queue_family = physical.queue_families().find(|&q| q.supports_graphics())
+                       .expect("couldn't find a graphical queue family");
+    let (device, mut queues) = {
+        Device::new(physical, &Features::none(), &DeviceExtensions::none(),
+        [(queue_family, 0.5)].iter().cloned()).expect("failed to create device")
+    };
+    let queue = queues.next().unwrap();
+
+    let image = StorageImage::new(device.clone(), Dimensions::Dim2d { width: 1024, height: 1024 },
+                                  Format::R8G8B8A8Unorm, Some(queue.family())).unwrap();
+    let buf = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(),
+                                             (0 .. 1024 * 1024 * 4).map(|_| 0u8))
+                                             .expect("failed to create buffer");
+    let command_buffer = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap()
+            .clear_color_image(image.clone(), ClearValue::Float([1.0, 1.0, 0.0, 1.0])).unwrap()
+            .copy_image_to_buffer(image.clone(), buf.clone()).unwrap()
+            .build().unwrap();
+    let finished = command_buffer.execute(queue.clone()).unwrap();
+    finished.then_signal_fence_and_flush().unwrap().wait(None).unwrap();
+    let buffer_content = buf.read().unwrap();
+    let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
+    image.save("image.png").unwrap();
+}
+
 fn main() {
     // copy_buffer();
-    hello_shader();
+    // hello_shader();
+    hello_image();
 }
